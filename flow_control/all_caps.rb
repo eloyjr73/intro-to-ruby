dcdb79060bc600789d5998b8944b99d5623e846b
Eloy Rios <eloyjr73@gmail.com>
2022-01-20 13:04:46 -0500

    fourth chapter finished

---
+++ flow_control/all_caps.rb
@@ -0,0 +1,4 @@
+def allcaps(input)
+  input.upcase
+end
+puts allcaps("hello") 
---
+++ flow_control/snippet_3.rb
@@ -0,0 +1,11 @@
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "alright"
+elsif (x + 1) >= (y)
+  puts "alright now"
+elsif (y +1) == x
+  puts "ALRIGHT NOW"
+else
+  puts "alrighty"
+end
