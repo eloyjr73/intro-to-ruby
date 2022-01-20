0b6c8cf236c09f78fbd96f0b667f27092f804d95
Eloy Rios <eloyjr73@gmail.com>
2022-01-20 14:22:05 -0500

    fifth chapter finish

---
+++ loop_and_iterators/countdown4.rb
@@ -0,0 +1,7 @@
+x = [1, 3, 4, 5, 5]
+
+for i in x.reverse do
+  puts i
+end
+
+puts "done!"
---
+++ loop_and_iterators/exercise2.rb
@@ -0,0 +1,5 @@
+x = nil
+while x != "STOP"
+  puts "hello would you like me to stop"
+  x = gets.chomp
+end
