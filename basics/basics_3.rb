c639924f0d02413768cb165f0096ecf54669f988
Eloy Rios <eloyjr73@gmail.com>
2022-01-19 22:52:32 -0500

    adding first chapter

---
+++ basics/basics_3.rb
@@ -0,0 +1,4 @@
+movies = { :akira => 1990,
+           :house => 1977,
+           :barry_lyndon => 1975}
+movies.each { |x, y| puts y }
---
+++ basics/basics_4.rb
@@ -0,0 +1,2 @@
+movies = [1990, 1975, 1977]
+movies.each {|x| puts x }
